322be4f244f5cef9f26be47dcec97b44388d78b5
Stephen Kelly <sbk13@my.fsu.edu>
2023-09-27 15:07:04 -0500

    added update to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 // next.config.js
 // next.config.js
 module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp3)$/,
